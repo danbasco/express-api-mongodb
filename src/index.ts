d6c94bc3c9ca49bc760dea13b2b64fe33b0caf08
Daniel Vasconcelos <79517613+danbasco@users.noreply.github.com>
2025-10-17 15:34:05 +0000

    refactor: typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+import database from "./database/configdb";
+
+dotenv.config();
+
+const app = express();
+database.connect();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "Hello World! Welcome to L-I-F-E." });
+});
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(PORT, () => {
+  console.log(`App is listening on http://localhost:${PORT}/`);
+});
